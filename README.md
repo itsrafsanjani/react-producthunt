d1905401e98f74baf776786149b8efd59df3234a
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-10-03 00:31:22 +0600

    feat: simple product hunt with react

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Product Hunt
+
+> Up vote a product.
+
+- Made with `Class` Component, `Semantic
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './assets/css/semantic.css'
 import './index.css'
+import './seed'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
