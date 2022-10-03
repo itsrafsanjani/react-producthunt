f406a9a0f18644e9dfead6e2ac40d50ea34652cf
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-10-03 21:19:47 +0600

    feat: semantic ui css import using npm

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './assets/css/semantic.css'
+import 'semantic-ui-css/semantic.min.css'
 import './index.css'
 import './seed'
 
